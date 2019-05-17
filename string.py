a9288fed6a17211853646e6a8321f2ffc7a7069c
Kouhei Yamada <kouhei@users.noreply.github.com>
2019-05-17 17:50:04 +0900

    update

--- string.py
+++ string.py
@@ -10,3 +10,13 @@ def zen_num_to_han_num(text):
     for i, zen_num in enumerate(zen_nums):
         text = text.replace(zen_num,str(i))
     return text
+
+def ngram(seq, n):
+    return [(seq[i:i+n])for i, j in enumerate(seq)if len(seq[i:i+n]) == n]
+
+if __name__ == "__main__":
+    words = "I am an NLPer"
+    list_ = words.split(" ")
+    ans_word = ngram(words, 2)
+    ans_list = ngram(list_, 2)
+    print(ans_word, ans_list)
